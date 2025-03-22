5704f21eb49701bc0588fafcc4f3d92b66e2cd2c
yancy <6546546@asd.com>
2025-03-22 19:39:43 +0800

    add markdown

--- command.sh
+++ command.sh
@@ -10,11 +10,12 @@ python main.py --config ./configs/VQA/train_qwen.yaml
 
 
 python main.py --config ./configs/VizWiz/train_qwen.yaml
+python main.py --config ./configs/VizWiz/train_llava.yaml
 
 python main.py --config /home/hallscope/configs/VQAIDK/train_qwen.yaml
+python main.py --config /home/hallscope/configs/VQAIDK/train_llava.yaml
 
 python main.py --config /home/hallscope/configs/self_eval/pope_adversarial.yaml
-
 
 python main.py --config /home/hallscope/configs/VQA/train_llava.yaml
 
